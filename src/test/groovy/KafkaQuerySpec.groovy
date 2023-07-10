4ce792c3d44ef060a48f0a213f2539b4b1518376
devatherock <devatherock@gmail.com>
2023-07-09 20:20:32 -0500

    fix: Upgraded dependencies to resolve security vulnerabilities

--- src/test/groovy/KafkaQuerySpec.groovy
+++ src/test/groovy/KafkaQuerySpec.groovy
@@ -1,15 +1,10 @@
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.serialization.StringSerializer
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Configuration
-import org.springframework.kafka.core.DefaultKafkaProducerFactory
-import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.test.EmbeddedKafkaBroker
 import org.springframework.kafka.test.context.EmbeddedKafka
-import org.springframework.kafka.test.utils.KafkaTestUtils
 
 import spock.lang.Specification
 
@@ -25,31 +20,21 @@ class KafkaQuerySpec extends Specification {
     @Rule
     TemporaryFolder temporaryFolder = new TemporaryFolder()
 
-    KafkaTemplate kafkaTemplate
     String topic = 'test-topic'
     GroovyShell shell = new GroovyShell(new Binding())
 
-    void setup() {
-        kafkaTemplate = initializeKafkaTemplate()
-    }
-
-    KafkaTemplate initializeKafkaTemplate() {
-        new KafkaTemplate<String, String>(new DefaultKafkaProducerFactory<>(
-                KafkaTestUtils.producerProps(embeddedKafka),
-                new StringSerializer(),
-                new StringSerializer()
-        ))
-    }
-
     void 'test query message by key'() {
         given:
-        kafkaTemplate.send(new ProducerRecord(
-                topic,
-                null,
-                'test',
-                'hello'
-        ))
+        File inputFile = temporaryFolder.newFile('input.txt')
+        inputFile << 'hello'
         File outputFile = temporaryFolder.newFile('output.txt')
+
+        and:
+        shell.run(new File('scripts/Groovy/kafka/KafkaPublisher.groovy'),
+                '-k', 'test',
+                '-b', embeddedKafka.getBrokersAsString(),
+                '-t', topic,
+                '-f', inputFile.getAbsolutePath())
 
         when:
         shell.run(new File('scripts/Groovy/kafka/KafkaQuery.groovy'),
